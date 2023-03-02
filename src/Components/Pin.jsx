7d207b652337fc69e72d5024de52570d281d2df8
Gerson Steven <98818664+Stevenjr1028@users.noreply.github.com>
2023-03-02 12:54:49 -0500

    Primer Avance Finalizado

--- src/Components/Pin.jsx
+++ src/Components/Pin.jsx
@@ -1,52 +1,57 @@
 import React, { useState } from "react";
 import "../CSS/Pin_Styles.css";
-import upload_img from "../Helpers/UpImage";
+import check_size from "../Helpers/check_size";
 
 
-function Pin() {
-    const [pinImage, setPinImage] = useState();
-    
+
+
+
+
+function Pin(props) {
+    //const [pinImage, setPinImage] = useState();
+
 
     return (
-        <div>
+        /*   <div>
+  
+               <input onChange={event => upload_img(event, setPinImage)} type="file" name="picture" id="picture" value=""></input>
+   
+              <div className={`card card_${props.pinDetails.pin_size}`}> */
+        <div className={`card card_${props.pinDetails.pin_size}`}>
+            <div className="pin_tittle">{props.pinDetails.title}</div>
 
-            <input onChange={event => upload_img(event, setPinImage)} type="file" name="picture" id="picture" value=""></input>
-
-            <div className="card">
-                <div className="pin_tittle"></div>
-
-                <div className="pin_modal">
-                    <div className="modal_head">
-                        <div className="save_card">Guardar</div>
-                    </div>
-
-
-                    <div className="modal_foot">
-                        <div className="destination">
-                            <div className="pint_mock_icon_container">
-                                <img src="./Images/up-right-arrow.png" alt="destination" className="pint_mock_icon" />
-                            </div>
-                            <span>Eatery</span>
-                        </div>
-
-                        <div className="pint_mock_icon_container">
-                            <img src="./Images/share.png" alt="send" className="pint_mock_icon" />
-                        </div>
-
-                        <div className="pint_mock_icon_container">
-                            <img src="./Images/options.png" alt="edit" className="pint_mock_icon" />
-                        </div>
-                    </div>
-
+            <div className="pin_modal">
+                <div className="modal_head">
+                    <div className="save_card">Guardar</div>
                 </div>
 
 
+                <div className="modal_foot">
+                    <div className="destination">
+                        <div className="pint_mock_icon_container">
+                            <img src="./Images/up-right-arrow.png" alt="destination" className="pint_mock_icon" />
+                        </div>
+                        <span>{props.pinDetails.destination}</span>
+                    </div>
 
-                <div className="pin_image">
-                    <img src={pinImage} alt="pin_image" />
+                    <div className="pint_mock_icon_container">
+                        <img src="./Images/share.png" alt="send" className="pint_mock_icon" />
+                    </div>
+
+                    <div className="pint_mock_icon_container">
+                        <img src="./Images/options.png" alt="edit" className="pint_mock_icon" />
+                    </div>
                 </div>
+
+            </div>
+
+
+
+            <div className="pin_image">
+                <img onLoad={check_size} src={props.pinDetails.img_blob} alt="pin_image" />
             </div>
         </div>
+
     );
 
 
