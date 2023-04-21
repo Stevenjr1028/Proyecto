21b3bd39131ad8de15ae4bb1f6986eeb76173a07
lepraki <daniel.maya@uao.edu.co>
2023-04-21 17:48:13 -0500

    Conexion a Api de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,29 @@ import FinalBoard from './Components/FinalBoard'
 import Login from './Pages/Login'
 import Header from './Components/Header'
 import Maimboard from './Components/Maimboard'
-
+import unsplash from './api/unsplash'
 function App() {
-  
 
+  const getImages = (term) =>{
+    return unsplash.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+
+      }
+    })
+    
+  }
+
+  const onSearchSubmit = (term) =>{
+    console.log("on search submit", term)
+    getImages(term)
+
+  }
+  
+  
   return (
    <>
-     <Header/>
+     <Header onSubmit={onSearchSubmit}/>
      <Maimboard/>
 
    </>
